19b036a5ef367376185cda9f541f5b43aa0349f3
Shreevedha Solutions <connect@shreevedha.co.in>
2026-08-02 01:34:29 +0530

    Fix Vercel 500 error by adding dist/** to includeFiles and wrapping Express handler

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
 import app from '../dist/server.js';
 
-export default app;
+export default function handler(req, res) {
+  if (typeof app === 'function') {
+    return app(req, res);
+  }
+  if (app && typeof app.default === 'function') {
+    return app.default(req, res);
+  }
+  return app(req, res);
+}
